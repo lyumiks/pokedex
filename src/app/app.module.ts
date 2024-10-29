bb6dcbc5ddd697cbedf5cd4278c99eb69c1c1d4f
Yumi Kato <leilayumi07@gmail.com>
2024-10-28 22:50:54 -0300

    chore(logo): inclui a logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { SearchBarComponent } from '../components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, PokemonCardComponent, SearchBarComponent], //Aqui vem os componentes
+  imports: [BrowserModule, AppRoutingModule], //Aqui vem só os módulos
   providers: [],
   bootstrap: [AppComponent],
 })
